2ea46129a443fe7dc9d0defb5ffe947e02b63825
SjDev <sjdev.front@gmail.com>
2024-04-06 10:40:23 +0200

    Add ressource

---
+++ client/client.lua
@@ -0,0 +1,24 @@
+Citizen.CreateThread(function()
+    while true do
+        local ped = PlayerPedId()
+        local players = GetActivePlayers()
+
+        local playerPeds = {}
+        for _, player in ipairs(players) do
+            playerPeds[player] = GetPlayerPed(player)
+        end
+
+        for player, playerPed in pairs(playerPeds) do
+            local veh = GetVehiclePedIsUsing(playerPed)
+            SetEntityNoCollisionEntity(ped, veh, true)
+        end
+
+        N_0x4757f00bc6323cfe(-1553120962, 0.0) 
+
+        Wait(25)
+    end
+end)
+
+git config --global user.name "SjDev"
+
+git config --global user.email "[email protected]"
---
+++ fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'cerulean'
+use_fxv2_oal 'yes'
+lua54 'yes'
+game 'gta5'
+version '1.0.3'
+
+client_script "client/client.lua"
